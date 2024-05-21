d727a61e8711d2b29e54bda3a027158aa9a8ebd8
VibesAround <ilya.patiev@gmail.com>
2024-05-21 19:23:13 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,8 +3,24 @@ import './Form.css'
 
 const Form = () => {
     return (
-        <div>
-            Form
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Город'}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Имя'}
+            />
+            <select className={'select'}>
+                <option value={'physical'}>Показал друг</option>
+                <option value={'physical'}>Рассылка в Телеграм</option>
+                <option value={'physical'}>Увидел в канале</option>
+                <option value={'physical'}>Интернет</option>
+            </select>
 
         </div>
     );
