19a06263001b58f66dd75a04742781ccf53c88b6
VibesAround <ilya.patiev@gmail.com>
2024-05-21 17:35:11 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className="header">
             <button onClick={onClose}>Закрыть</button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?}
+                {tg.initDataUnsafe?.user?.username}
             </span>
             
         </div>
